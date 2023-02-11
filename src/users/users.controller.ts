2c3098d41efcd9cff4f354244c6a29562f24278c
stefanj122 <stefan.jeftic122@gmail.com>
2023-02-11 20:11:24 +0100

    add singup body for swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateUserDto } from './dto/createUserDtio.dto';
+import { LoginUserDto } from './dto/loginUserDto.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('/users')
 export class UsersController {
   constructor(
@@ -11,12 +14,13 @@ export class UsersController {
     private readonly authService: AuthService,
   ) {}
 
-  @Post()
+  @Post('/singup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
 
   @UseGuards(AuthGuard('local'))
+  @ApiBody({ type: LoginUserDto })
   @Post('/login')
   async login(@Req() req: any) {
     return await this.authService.login(req.user);
