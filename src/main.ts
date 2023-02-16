24d3b228d15eb5b171a60693cb3f39221afed440
stefanj122 <stefan.jeftic122@gmail.com>
2023-02-16 17:35:00 +0100

    Change swagger config

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Fotodesk')
+    .setDescription('Platform for uploads pictures')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('fotodesk')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
