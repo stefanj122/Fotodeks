d63e782bba9796318ff1612d24844ebc0f181282
DrugBoban <boban_bn@hotmail.com>
2023-02-21 17:13:25 +0100

    Update package-lock.json, package.json, and images.service.ts

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { existsSync, mkdirSync } from 'fs';
+import { basename, join } from 'path';
 import { Images } from 'src/entity/image.entity';
 import { Repository } from 'typeorm';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class ImagesService {
@@ -9,4 +12,24 @@ export class ImagesService {
     @InjectRepository(Images)
     private readonly imagesRepository: Repository<Images>,
   ) {}
+
+  async generateThumbnail(path: string, thumbsize: string): Promise<any> {
+
+    const imageName = basename(path);
+    const image = await this.imagesRepository.findOneBy({ name: imageName });
+    const thumbnailPath = join(process.cwd(), "public/images", `${image.id}`, thumbsize);
+
+    if(existsSync(thumbnailPath)) {
+      return true;
+    }
+    mkdirSync(thumbnailPath, {recursive: true});
+
+    const WxH = thumbsize.split('x');
+
+    sharp(path).resize(
+      Number(WxH[0]), 
+      Number(WxH[1]),
+    )
+    .toFile(join(thumbnailPath, imageName))
+  }
 }
