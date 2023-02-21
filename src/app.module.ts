077cfa10890952a33bea107fc6b507da00d53ac7
zanfirovic.mz <zanfirovic.mz@gmail.com>
2023-02-21 18:05:37 +0100

    Update package-lock.json, package.json, and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImagesModule } from './images/images.module';
 import { WatermarksModule } from './watermarks/watermarks.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      port: parseInt(process.env.DATABASE_PORT),
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'fotodesk_v2',
+      port: 3306,
       entities: ['./dist/entitie/*.entitie.js'],
       synchronize: true,
       migrations: ['./dist/migration/*.js'],
