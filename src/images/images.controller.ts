697afe90c0c8454f96d2e5fbe524e81b4b88f8df
stefanj122 <stefan.jeftic122@gmail.com>
2023-02-21 09:57:21 +0100

    Endpoint for images upload and generated thumbnails 285x190

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,9 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
+import { imagesStorage } from 'src/config/multer.config';
 import { ImagesService } from './images.service';
 
 @ApiTags('images')
 @Controller('/images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('images', 30, imagesStorage))
+  async uploadImages(
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({
+            fileType: /png|jpeg|jpg/g,
+          }),
+        ],
+      }),
+    )
+    images: Array<Express.Multer.File>,
+  ): Promise<{ data: { id: number; name: string; path: string }[] }> {
+    return await this.imagesService.uploadImages(images);
+  }
 }
