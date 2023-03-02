ff5cf7c4df0b1057c28f3515ba45067820dbad38
Bane <bane.popic1998@gmail.com>
2023-03-02 20:59:15 +0100

    Update user.controller.ts

--- src/admin/user/user.controller.ts
+++ src/admin/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ForbiddenException, Get} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/decorator/get-user.decorator';
 import { UserService } from './user.service';
 
 @ApiTags('admin-user')
 @Controller('/admin/user')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
+
+  @Get('/me')
+  async getCurrentUser(@GetUser() user){
+    console.log(user);
+    if (user){
+      delete user.password;
+      return user;
+    }
+    throw new ForbiddenException()
+  }
 }
