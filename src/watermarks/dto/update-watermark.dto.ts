3c007b646dabe8c56ae0e2b9c655ef02f396bd20
miroslavrs81 <miroslav81ilic@gmail.com>
2023-03-07 22:00:22 +0100

    Delete update-watermark.dto.ts

--- src/watermarks/dto/update-watermark.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { CreateWatermarkDto } from './create-watermark.dto';
-
-export class UpdateWatermarkDto extends PartialType(CreateWatermarkDto) {
-  @ApiProperty()
-  description: string;
-}
