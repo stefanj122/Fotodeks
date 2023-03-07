9945ea2a420434c25756db1c1a6655d88aed61e9
zanfirovic.mz <zanfirovic.mz@gmail.com>
2023-03-07 20:00:22 +0100

    Update images.controller.ts and images.service.ts

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ImagesService } from './images.service';
 
 @ApiTags('images')
 @Controller('/images')
 export class ImagesController {
-  constructor(private readonly userService: ImagesService) {}
+  constructor(private readonly imagesService: ImagesService) {}
+  @Put()
+  async updateImagesTags(@Body() imagesDataTags: {id: number; tags: string}[]){
+    return await this.imagesService.updateImagesTags(imagesDataTags)
+  }
+
 }
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Images } from 'src/entity/image.entity';
 import { Repository } from 'typeorm';
@@ -9,4 +9,17 @@ export class ImagesService {
     @InjectRepository(Images)
     private readonly imagesRepository: Repository<Images>,
   ) {}
-}
+    async updateImagesTags(imagesDataTags: { id: number; tags: string} []){
+    const arrOfPromises = []
+    imagesDataTags.forEach((element) => {
+      arrOfPromises.push(this.imagesRepository.update(
+        element.id, {
+          tags: element.tags
+        })
+      );
+    })
+    try { await Promise.all(arrOfPromises)
+      return 'success'}
+      catch(error){throw new BadRequestException()}
+   }
+  }
