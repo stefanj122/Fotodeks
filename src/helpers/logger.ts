235aed8d77e2625d11f5a84498d804d97643efe1
miroslavrs81 <miroslav81ilic@gmail.com>
2023-04-08 20:57:39 +0200

    Update logger.ts

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,24 +1,17 @@
 import { createLogger, transports, format } from 'winston';
+const { combine, colorize, timestamp, json } = format;
 
 export const logger = createLogger({
   transports: [
     new transports.File({
       filename: 'notifications-error.log',
       level: 'error',
-      format: format.combine(
-        format.colorize(),
-        format.timestamp(),
-        format.json(),
-      ),
+      format: combine(colorize(), timestamp(), json()),
     }),
     new transports.File({
       filename: 'notifications.log',
       level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.timestamp(),
-        format.json(),
-      ),
+      format: combine(colorize(), timestamp(), json()),
     }),
   ],
 });
